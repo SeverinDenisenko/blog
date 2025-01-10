7cf2eea086547cc85193c8e5442bd626a8be762b
Severin Denisenko <81779013+SeverinDenisenko@users.noreply.github.com>
2025-01-10 18:59:00 +0300

    Add http requests proper parsing

--- src/Server.hs
+++ src/Server.hs
@@ -6,14 +6,20 @@ module Server
   )
 where
 
-import Control.Exception (IOException, SomeException, catch, try)
+import Control.Exception (Exception, IOException, SomeException, catch, try)
+import Control.Exception.Base (throw)
 import Data.ByteString.Char8 (ByteString, length, pack, unpack)
 import Network.Socket
 import qualified Network.Socket.ByteString as SocketByteString
 import System.FilePath
 import System.IO
 
-data ServerConfig = ServerConfig {server_port :: Int, server_connection_pool :: Int, server_content :: [Char], default_page :: [Char]}
+data ServerConfig = ServerConfig
+  { server_port :: Int,
+    server_connection_pool :: Int,
+    server_content :: [Char],
+    default_page :: [Char]
+  }
 
 runServer :: ServerConfig -> IO ()
 runServer server_config = do
@@ -50,31 +56,76 @@ writeSocket csock string = catch (SocketByteString.send csock (pack string)) han
       print ex
       return 0
 
+closeConnection :: Socket -> IO ()
+closeConnection csock = close csock
+
 dumpFileContents :: [Char] -> IO [Char]
 dumpFileContents name = do
   fileHandle <- openFile name ReadMode
   hGetContents fileHandle
 
-createHttpResponce :: [Char] -> [Char]
-createHttpResponce str = "HTTP/1.1 200 OK\r\n" ++ "Content-Length: " ++ show (Prelude.length str) ++ "\r\n" ++ "Content-Type: text/html; charset=utf-8\r\n" ++ "\r\n" ++ str
+createHttpResponse :: [Char] -> [Char]
+createHttpResponse str = "HTTP/1.1 200 OK\r\n" ++ "Content-Length: " ++ show (Prelude.length str) ++ "\r\n" ++ "Content-Type: text/html; charset=utf-8\r\n" ++ "\r\n" ++ str
 
-parceHttpRequest :: [Char] -> ServerConfig -> [Char]
-parceHttpRequest header server_config = do
-  let header_terms = words header
-  let file_name = if Prelude.length header_terms >= 2 then header_terms !! 1 else default_page server_config
-  takeDirectory (server_content server_config) </> takeFileName file_name
+data HTTPException = HTTPException deriving (Show)
+
+instance Exception HTTPException
+
+data HTTPRequest = HTTPRequest
+  { http_method :: [Char],
+    http_path :: [Char],
+    http_request_protocol_version :: [Char]
+  }
+
+data HTTPResponse = HTTPResponse
+  { http_response_protocol_version :: [Char],
+    http_response_code :: [Char],
+    http_response_status :: [Char],
+    http_response_content_length :: Int,
+    http_response_content_type :: [Char]
+  }
+
+parceHttpRequest :: [Char] -> HTTPRequest
+parceHttpRequest request = do
+  let request_tokens = words request
+  if Prelude.length request_tokens < 3
+    then throw HTTPException
+    else do
+      let method = request_tokens !! 0
+      let path = request_tokens !! 1
+      let protocol_version = request_tokens !! 2
+      HTTPRequest method path protocol_version
+
+getResponseFilePath :: [Char] -> ServerConfig -> [Char]
+getResponseFilePath path server_config = takeDirectory (server_content server_config) </> takeFileName path
+
+createGETResponse :: Socket -> ServerConfig -> HTTPRequest -> IO ()
+createGETResponse csock server_config request = do
+  let response_file = getResponseFilePath (http_path request) server_config
+  file_dump_try <- try (dumpFileContents response_file) :: IO (Either SomeException [Char])
+  case file_dump_try of
+    Left ex -> do
+      print ex
+      closeConnection csock
+      return ()
+    Right file_dump -> do
+      let http_response = createHttpResponse file_dump
+      _ <- writeSocket csock http_response
+      handleRequest csock server_config
+
+createResponse :: Socket -> ServerConfig -> HTTPRequest -> IO ()
+createResponse csock server_config request
+  | http_method request == "GET" = createGETResponse csock server_config request
+  | http_method request == "POST" = closeConnection csock
+  | http_method request == "PUT" = closeConnection csock
+  | http_method request == "DEL" = closeConnection csock
+  | otherwise = closeConnection csock
 
 handleRequest :: Socket -> ServerConfig -> IO ()
 handleRequest csock server_config = do
   dat <- readSocket csock
-  let filename = parceHttpRequest (unpack dat) server_config
-  file_dump_try <- try (dumpFileContents filename) :: IO (Either SomeException [Char])
-  case file_dump_try of
-    Left ex -> do
-      print ex
-      close csock
-      return ()
-    Right file_dump -> do
-      let http_responce = createHttpResponce file_dump
-      _ <- writeSocket csock http_responce
-      if Data.ByteString.Char8.length dat == 0 then close csock else handleRequest csock server_config
+  if Data.ByteString.Char8.length dat == 0
+    then close csock
+    else do
+      let request = parceHttpRequest (unpack dat)
+      createResponse csock server_config request
